00cae33895ee778a3f7410792080ca251a7a26e9
rajesh2024-krita <rajesh.kritatechnosolutions@gmail.com>
2025-11-11 14:10:02 +0530

    upload memory limit added

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ app.use(
 );
 app.use(express.json());
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/logs", activityLogs);
